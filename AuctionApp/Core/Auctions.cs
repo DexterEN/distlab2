26f6c0e92ea5148a421737755b54224291d88da7
Dexter Nyika <129272732+DexterEN@users.noreply.github.com>
2024-10-10 12:05:39 +0200

    init commit

---
+++ AuctionApp/Core/Auctions.cs
@@ -0,0 +1,48 @@
+﻿namespace WebApplication1.Core;
+
+public class Auctions
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Description { get; set; }
+    public string UserName { get; set; }
+    
+    private List<Bid> _bids = new List<Bid>();
+    public IEnumerable<Bid> Bids => _bids;
+
+    public Auctions(string title, DateTime endDate, string description, string userName)
+    {
+        Title = title;
+        EndDate = endDate;
+        Description = description;
+        UserName = userName;
+    }
+
+    public Auctions(int id, string title, DateTime endDate, string description, string userName)
+    {
+        Id = id;
+        Title = title;
+        EndDate = endDate;  
+        Description = description;
+        UserName = userName;
+    }
+    
+    public Auctions( ){ }
+
+    public void AddBid(Bid bid)
+    {
+        _bids.Add(bid);
+    }
+
+    public bool isOngiong()
+    {
+        return EndDate < DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id}: {Title}: {Description} created by {UserName} ends: {EndDate}";
+    }
+    
+}
---
+++ AuctionApp/Core/Bid.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Core;
+
+public class Bid
+{
+    public int Id { get; set; }
+    public int Amount { get; set; }
+    public string UserName { get; set; }
+    
+}
